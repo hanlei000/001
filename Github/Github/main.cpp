75eefe47abb08761ae5eded025f7b2af25bc251e
hanlei000 <3283152875@qq.com>
2020-11-25 23:33:23 +0800

    1

--- Github/Github/main.cpp
+++ Github/Github/main.cpp
@@ -1,33 +1,10 @@
-/*#include <stdio.h>
-void q()
-{
-	int i, j, n = 0, a, b = 0;
-	for (a = 1; a <= 3000; a++)
-	{
-		for (i = 1; i < a; i++)
-		{
-			if (a % i == 0)
-				b = b + i;
+#include <stdio.h>
 
-
-		}
-		if (b <= 3000 && b > a)
-		{
-			for (j = 1; j < b; j++)
-			{
-				if (b % j == 0)
-					n = n + j;
-			}
-
-		}
-		if (a == n)
-			printf("(%d,%d)", a, b);
-		n = 0, b = 0;
-
-	}
+int main() {
+	int a;
+	a = 2, 3, 4, 5;
+	printf("%d\n", a);
+	a = (2, 3, 4, 5);
+	printf("%d\n", a);
+	return 0;
 }
-	int main()
-	{
-		q();
-		return 0;
-	}*/
